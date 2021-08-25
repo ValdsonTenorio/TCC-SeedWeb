532dc2c414ca0132841939c956ff5dc0b96c9526
Valdson <valdsonmartinstenorio721@gmail.com>
2021-08-25 16:57:54 -0400

    Correção do Css login part2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,21 +38,8 @@
                              <div class="line"></div>
                          </div> 
                         </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-3 ">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-2">
+                        <div class="row">
+                            <div class="col-12">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
@@ -63,7 +50,19 @@
                                     </a>
                                 @endif
                             </div>
+                            <div class="col mt-3">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
                         </div>
+                        
+
+                        
                     </form>
                 </div>
 
