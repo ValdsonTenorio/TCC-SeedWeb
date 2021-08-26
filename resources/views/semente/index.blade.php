9b3b262988454a8427f808aeda57a77d3251e1cb
Misael167 <misagamebr@gmail.com>
2021-08-26 15:48:21 -0300

    Complementado as informações da semente

--- resources/views/semente/index.blade.php
+++ resources/views/semente/index.blade.php
@@ -17,7 +17,7 @@
 	<div class="row w-100">
 		@endif
 		<div class="col-12 col-md text-center">
-			<img class="bd-placeholder-img rounded-circle" width="140" height="140" src="{{ Voyager::image( $semente->imagem ) }}">	
+			<img class="bd-placeholder-img rounded-circle" width="140" height="140" src="{{ Voyager::image( $semente->imagem ) }}">
 
 			<h2>{{ $semente->nome_popular }}</h2>
 			<p>{!! $semente->nome_cientifico !!}</p>
@@ -29,7 +29,7 @@
 	@endforeach
 	<div class="row">
 	<div class="pagination-style">
-		
+
 			<div class="col-md-12">
 				<?php echo $sementes->render(); ?>
 			</div>
