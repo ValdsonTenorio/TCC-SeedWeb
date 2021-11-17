39c00a2bb11b011558affa6ddc3b8ce1236c5c05
Valdson Martins <valdsonmartinstenorio721@gmail.com>
2021-11-17 14:49:07 -0400

    correçoes merge 2

--- routes/web.php
+++ routes/web.php
@@ -34,17 +34,13 @@ Route::get('/pesquisador/search', [App\Http\Controllers\PesquisadorController::c
 Route::get('/pesquisador', [App\Http\Controllers\PesquisadorController::class, 'index'])->name('pesquisador.index');
 Route::get('/pesquisador/create', [App\Http\Controllers\PesquisadorController::class, 'create'])->name('pesquisador.create');
 Route::post('/pesquisador/store', [App\Http\Controllers\PesquisadorController::class, 'store'])->name('pesquisador.store');
-<<<<<<< HEAD
 Route::post('/pesquisador/aprovar', [App\Http\Controllers\PesquisadorController::class, 'aprovar'])->name('pesquisador.aprovar');
 Route::put('/pesquisador/negar', [App\Http\Controllers\PesquisadorController::class, 'negar'])->name('pesquisador.negar');
-=======
 Route::get('/pesquisador/solicitacoes', [App\Http\Controllers\PesquisadorController::class, 'solicitacoes'])->name('pesquisador.solicitacoes');
->>>>>>> a16b9501641c5ac51949866be908f0512e7cc9be
 Route::get('/pesquisador/{id}', [App\Http\Controllers\PesquisadorController::class, 'show'])->name('pesquisador.show');
 Route::get('/pesquisador/edit/{id}', [App\Http\Controllers\PesquisadorController::class, 'edit'])->name('pesquisador.edit');
 Route::put('/pesquisador/{id}', [App\Http\Controllers\PesquisadorController::class, 'update'])->name('pesquisador.update');
 Route::delete('/pesquisador/{id}', [App\Http\Controllers\PesquisadorController::class, 'destroy'])->name('pesquisador.destroy');
-
 Route::get('/semente/search', [App\Http\Controllers\SementeController::class, 'search'])->name('semente.search');
 Route::get('/semente', [App\Http\Controllers\SementeController::class, 'index'])->name('semente.index');
 Route::get('/semente/create', [App\Http\Controllers\SementeController::class, 'create'])->name('semente.create');
